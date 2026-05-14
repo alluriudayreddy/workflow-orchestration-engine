81113fa2e45fec9104923b9205149e680f388d05
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-14 16:31:49 +0530

    connect engine.py to workflow_manager.py

--- engine.py
+++ engine.py
@@ -1,14 +1,15 @@
-class WorkflowEngine:
+from workflow_manager import WorkflowManager
+
+class workflowEngine:
+
+    def __init__(self):
+        self.manager = WorkflowManager()
+
     def run_workflow(self, workflow_name):
+        workflow = self.manager.get_workflow(workflow_name)
 
-        if workflow_name == "backup":
-            print("Starting Backup Workflow...")
-
-        elif workflow_name == "data_processing":
-            print("Starting Data Processing Workflow...")
-
-        elif workflow_name == "deployment":
-            print("Starting Deployment Workflow...")
+        if workflow:
+            print(f"Starting {workflow}...")
 
         else:
             print("Workflow not found.")
