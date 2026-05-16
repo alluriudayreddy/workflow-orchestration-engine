2fb107faf360f9044e832fe68aa0093e83279535
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-16 11:19:37 +0530

    connect data processing and deployment worklfows structure into workflow_manager.py

--- workflow_manager.py
+++ workflow_manager.py
@@ -1,11 +1,15 @@
 from workflows.backup_workflow import backup_workflow
+from workflows.data_processing_workflow import data_processing_workflow
+from workflows.deployment_workflow import deployment_workflow
 
 class WorkflowManager:
     
     def __init__(self):
 
         self.workflows = {
-            "backup": backup_workflow
+            "backup": backup_workflow,
+            "data_processing": data_processing_workflow,
+            "deployment": deployment_workflow
         }
 
     def get_workflow(self, workflow_name):
