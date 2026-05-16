00f7707e4f0d07869f37f408c704378fc42ced46
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-17 00:05:44 +0530

    create a good format for log results

--- helpers.py
+++ helpers.py
@@ -1,4 +1,10 @@
-def write_log(message):
-    
+from datetime import datetime
+
+def write_log(message, level="INFO"):
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    formatted_log = f"[{timestamp}] [{level}] [{message}]"
+
     with open("logs/execution_log.txt", "a") as log_file:
-        log_file.write(message + "\n")
+        log_file.write(formatted_log + "\n")
