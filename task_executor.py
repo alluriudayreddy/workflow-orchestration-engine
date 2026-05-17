3d3861e35a8e9b226f5d9902f794126af51dac80
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-17 15:07:04 +0530

    add workflow failure handling system

--- task_executor.py
+++ task_executor.py
@@ -1,4 +1,7 @@
 class TaskExecutor:
 
     def execute_task(self, task):
+
+        if task == "Compress Files":
+            raise Exception("Compression Failed")
         print(f"Executing Task: {task}")
