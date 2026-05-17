40c06698cd6c70c6d41e61714ecd18d5e4fc4fb4
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-05-17 15:32:04 +0530

    add live workflow status update

--- engine.py
+++ engine.py
@@ -3,31 +3,53 @@ from status_tracker import StatusTracker
 from task_executor import TaskExecutor
 from helpers import write_log
 
+
 class WorkflowEngine:
 
     def __init__(self):
-        self.executor = TaskExecutor()
 
+        self.executor = TaskExecutor()
         self.manager = WorkflowManager()
         self.tracker = StatusTracker()
 
+
     def run_workflow(self, workflow_name):
+
         workflow = self.manager.get_workflow(workflow_name)
 
         if workflow:
-            self.tracker.set_status(workflow_name, "running")
 
-            print(f"\nStarting {workflow_name} workflow...\n")
-            write_log(f"Starting {workflow_name} workflow...")
+            try:
 
-            for task in workflow:
-                self.executor.execute_task(task)
-                write_log(f"Executing Task: {task}")
+                self.tracker.set_status(workflow_name, "running")
+                print(f"Workflow Status: {self.tracker.get_status(workflow_name)}")
 
-            self.tracker.set_status(workflow_name, "completed")
-            write_log(f"{workflow_name} workflow completed")
+                print(f"\nStarting {workflow_name} workflow...\n")
+                write_log(f"Starting {workflow_name} workflow...", "INFO")
 
-            print(f"\nWorkflow Status: {self.tracker.get_status(workflow_name)}")
+                for task in workflow:
+
+                    self.executor.execute_task(task)
+
+                    write_log(f"Executing Task: {task}", "INFO")
+
+                self.tracker.set_status(workflow_name, "completed")
+
+                write_log(f"{workflow_name} workflow completed", "SUCCESS")
+
+                print(f"\nWorkflow Status: {self.tracker.get_status(workflow_name)}")
+
+            except Exception as error:
+
+                self.tracker.set_status(workflow_name, "failed")
+                print(f"Worklfow Status: {self.tracker.get_status(workflow_name)}")
+
+                print(f"\nWorkflow Failed: {error}")
+
+                write_log(f"{workflow_name} workflow failed: {error}", "ERROR")
 
         else:
+
             print("workflow not found.")
+
+            write_log(f"Workflow {workflow_name} not found", "ERROR")
